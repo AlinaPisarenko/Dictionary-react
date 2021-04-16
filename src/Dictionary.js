91724df5c89b24d062e9ad07086b47c6d287594b
AlinaPisarenko <78824916+AlinaPisarenko@users.noreply.github.com>
2021-04-16 13:02:23 -0400

    added default word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState(null);
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -15,17 +16,44 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleSubmit);
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function handleSearch(event) {
+    event.preventDefault();
+    search();
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSearch} class="input-group mb-3">
+          <input
+            type="search"
+            class="form-control"
+            placeholder="Type a word"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+            onChange={handleKeywordChange}
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+          >
+            Button
+          </button>
+        </form>
+
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
 }
--- src/Results.js
+++ src/Results.js
@@ -4,20 +4,29 @@ import Phonetic from "./Phonetic";
 import "./Results.css";
 
 export default function Results(props) {
-  console.log(props);
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <div class="row">
+          <section className="col-4">
+            <div className="Results-info">
+              <h2>{props.results.word}</h2>
 
-        <Phonetic phonetic={props.results.phonetics[0]} />
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
+              <Phonetic phonetic={props.results.phonetics[0]} />
             </div>
-          );
-        })}
+          </section>
+          <section className="col-8">
+            <div className="Results-info">
+              {props.results.meanings.map(function (meaning, index) {
+                return (
+                  <div key={index}>
+                    <Meaning meaning={meaning} />
+                  </div>
+                );
+              })}
+            </div>
+          </section>
+        </div>
       </div>
     );
   } else {
